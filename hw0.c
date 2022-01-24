8adb12d9eec5242e429f28516190d59dbe015a86
Chayanon Wichitrnithed <chayanon@laura.oden.utexas.edu>
2022-01-24 15:23:22 -0600

    Finish skeleton

--- hw0.c
+++ hw0.c
@@ -1,5 +1,6 @@
 #include <stdlib.h>
 #include <stdio.h>
+#include <time.h>
 
 void initialize(int n, float (*arr)[n]) {
   for (int i = 0; i < n; i++) {
@@ -19,6 +20,26 @@ void printArray(int n, float (*arr)[n]) {
 }
 
 void smooth(int n, float (*A)[n], float (*B)[n], float a, float b, float c) {
+  for (int i = 1; i < n-1; i++) {
+    for (int j = 1; j < n-1; j++) {
+      B[i][j] = a*(A[i-1][j-1] + A[i-1][j+1] + A[i+1][j-1] + A[i+1][j+1]) +
+        b*(A[i-1][j] + A[i+1][j] + A[i][j-1] + A[i][j+1]) +
+        c*A[i][j];
+    }
+  }
+}
+
+void count(int n, float (*A)[n], float t, int* out) {
+  int count = 0;
+
+  for (int i = 1; i < n-1; i++) {
+    for (int j = 1; j < n-1; j++) {
+      if (A[i][j] < t) {
+        count = count + 1;
+      }
+    }
+  }
+  *out = count;
 }
 
 int main() {
@@ -29,14 +50,18 @@ int main() {
   //int n = 16384 + 2;
   int n = 5;
 
+  int countX, countY = 0;
+
   // Allocate x and y to nxn
   float (*x)[n] = malloc(sizeof(*x) * n);
   float (*y)[n] = malloc(sizeof(*y) * n);
 
   initialize(n, x);
-  initialize(n, y);
+  smooth(n, x, y, a, b, c);
+  count(n, x, t, &countX);
+  count(n, y, t, &countY);
 
-  printArray(n, x);
+  //printArray(n, x);
 
   return 0;
 
